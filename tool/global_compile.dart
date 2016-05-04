d871c0a593af7fe13efa5321fe4641175c287511
Vijay Menon <vsm@google.com>
2016-05-04 14:29:57 -0700

    Package-level compilation

--- tool/global_compile.dart
+++ tool/global_compile.dart
@@ -13,6 +13,8 @@ import 'package:dev_compiler/src/analyzer/context.dart'
     show createAnalysisContextWithSources, AnalyzerOptions;
 import 'package:path/path.dart' as path;
 
+const ENTRY = "main";
+
 void main(List<String> args) {
   // Parse flags.
   var parser = new ArgParser()
@@ -25,63 +27,141 @@ void main(List<String> args) {
     throw 'Expected a single dart entrypoint.';
   }
   var entry = options.rest.first;
-  var outfile = options['out'];
-  var packageRoot = options['package-root'];
+  var outfile = options['out'] as String;
+  var packageRoot = options['package-root'] as String;
   var unsafe = options['unsafe-force-compile'];
 
   // Build an invocation to dartdevc.
   var dartPath = Platform.resolvedExecutable;
   var ddcPath = path.dirname(path.dirname(Platform.script.toFilePath()));
-  var command = [
+  var template = [
     '$ddcPath/bin/dartdevc.dart',
     'compile',
     '--no-source-map', // Invalid as we're just concatenating files below
     '-p',
-    packageRoot,
-    '-o',
-    outfile
+    packageRoot
   ];
   if (unsafe) {
-    command.add('--unsafe-force-compile');
+    template.add('--unsafe-force-compile');
   }
 
   // Compute the transitive closure
   var watch = new Stopwatch()..start();
   var context = createAnalysisContextWithSources(new AnalyzerOptions());
-  var inputSet = new Set<String>();
-  transitiveFiles(inputSet, context, entry, Directory.current.path);
-  command.addAll(inputSet);
-  var result = Process.runSync(dartPath, command);
 
-  if (result.exitCode == 0) {
-    print(result.stdout);
-  } else {
-    print('ERROR:');
-    print(result.stdout);
-    print(result.stderr);
-    exit(1);
-  }
-  var time = watch.elapsedMilliseconds / 1000;
-  print('Successfully compiled ${inputSet.length} files in $time seconds');
+  transitiveFiles(context, entry, Directory.current.path);
+  orderModules();
 
   // Prepend Dart runtime files to the output.
   var out = new File(outfile);
-  var code = out.readAsStringSync();
   var dartLibrary =
       new File(path.join(ddcPath, 'lib', 'runtime', 'dart_library.js'))
           .readAsStringSync();
+  out.writeAsStringSync(dartLibrary);
   var dartSdk = new File(path.join(ddcPath, 'lib', 'runtime', 'dart_sdk.js'))
       .readAsStringSync();
-  out.writeAsStringSync(dartLibrary);
   out.writeAsStringSync(dartSdk, mode: FileMode.APPEND);
-  out.writeAsStringSync(code, mode: FileMode.APPEND);
+
+  // Build modules
+  var tmpdir = Directory.systemTemp.createTempSync(outfile.replaceAll(path.separator, '__'));
+  for (var module in orderedModules) {
+    var file = tmpdir.path + path.separator + module + '.js';
+    var command = new List.from(template)
+      ..addAll(['-o', file]);
+    var dependences = new Set<String>();
+    computeDependences(module, dependences);
+    for (var dependence in dependences) {
+      var summary = tmpdir.path + path.separator + dependence + '.sum';
+      command.addAll(['-s', summary]);
+    }
+    var infiles = fileMap[module];
+    command.addAll(infiles);
+    var result = Process.runSync(dartPath, command);
+    if (result.exitCode != 0) {
+      print('ERROR: compiling $module');
+      print(result.stdout);
+      print(result.stderr);
+      out.deleteSync();
+      exit(1);
+    }
+    print('Compiled $module (${infiles.length} files)');
+    print(result.stdout);
+
+    // Append module
+    var codefile = new File(file);
+    var code = codefile.readAsStringSync();
+    out.writeAsStringSync(code, mode: FileMode.APPEND);
+  }
+
+  var time = watch.elapsedMilliseconds / 1000;
+  print('Successfully compiled ${inputSet.length} files in $time seconds');
 
   // Append the entry point invocation.
-  var moduleName = path.basenameWithoutExtension(outfile);
   var libraryName =
       path.withoutExtension(entry).replaceAll(path.separator, '__');
-  out.writeAsStringSync('dart_library.start("$moduleName", "$libraryName");\n',
+  out.writeAsStringSync('dart_library.start("$ENTRY", "$libraryName");\n',
       mode: FileMode.APPEND);
+}
+
+final inputSet = new Set<String>();
+final dependenceMap = new Map<String, Set<String>>();
+final fileMap = new Map<String, Set<String>>();
+
+final orderedModules = new List<String>();
+
+void orderModules([String module = ENTRY, Set<String> stack]) {
+  if (stack == null) {
+    stack = new Set<String>();
+  }
+  if (stack.contains(module)) {
+    throw 'Circular dependence on $module';
+  }
+  stack.add(module);
+  var dependences = dependenceMap[module];
+  if (dependences != null) {
+    for (var dependence in dependences) {
+      orderModules(dependence, stack);
+    }
+  }
+  orderedModules.add(module);
+  stack.remove(module);
+}
+
+void computeDependences(String module, Set<String> allDependences) {
+  if (dependenceMap.containsKey(module)) {
+    for (var dependence in dependenceMap[module]) {
+      if (allDependences.add(dependence)) {
+        computeDependences(dependence, allDependences);
+      }
+    }
+  }
+}
+
+String getModule(String uri) {
+  var sourceUri = Uri.parse(uri);
+  if (sourceUri.scheme == 'dart') {
+    return 'dart';
+  } else if (sourceUri.scheme == 'package') {
+    return path.split(sourceUri.path)[0];
+  } else {
+    return ENTRY;
+  }
+}
+
+bool processFile(String file) {
+  inputSet.add(file);
+
+  var module = getModule(file);
+  fileMap.putIfAbsent(module, () => new Set<String>());
+  return fileMap[module].add(file);
+}
+
+void processDependence(String from, String to) {
+  var fromModule = getModule(from);
+  var toModule = getModule(to);
+  if (fromModule == toModule || toModule == 'dart') return;
+  dependenceMap.putIfAbsent(fromModule, () => new Set<String>());
+  dependenceMap[fromModule].add(toModule);
 }
 
 String canonicalize(String uri, String root) {
@@ -93,12 +173,11 @@ String canonicalize(String uri, String root) {
   return sourceUri.toString();
 }
 
-void transitiveFiles(Set<String> results, AnalysisContext context,
-    String entryPoint, String root) {
+void transitiveFiles(AnalysisContext context, String entryPoint, String root) {
   entryPoint = canonicalize(entryPoint, root);
   if (entryPoint.startsWith('dart:')) return;
   var entryDir = path.dirname(entryPoint);
-  if (results.add(entryPoint)) {
+  if (processFile(entryPoint)) {
     // Process this
     var source = context.sourceFactory.forUri(entryPoint);
     if (source == null) {
@@ -108,13 +187,15 @@ void transitiveFiles(Set<String> results, AnalysisContext context,
     var library = context.computeLibraryElement(source);
     for (var entry in library.imports) {
       if (entry.uri == null) continue;
-      transitiveFiles(results, context, entry.uri, entryDir);
+      processDependence(entryPoint, canonicalize(entry.uri, entryDir));
+      transitiveFiles(context, entry.uri, entryDir);
     }
     for (var entry in library.exports) {
-      transitiveFiles(results, context, entry.uri, entryDir);
+      processDependence(entryPoint, canonicalize(entry.uri, entryDir));
+      transitiveFiles(context, entry.uri, entryDir);
     }
     for (var part in library.parts) {
-      results.add(canonicalize(part.uri, entryDir));
+      processFile(canonicalize(part.uri, entryDir));
     }
   }
 }
